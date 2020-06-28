d5a88a306d58c518bec28baeaaa9d8ad6a9857be
GuGaYoung <rkdudda12@naver.com>
2020-06-28 17:01:52 +0900

    회원가입 시 user의 수로 idx의 값이 적용되도록 수정

--- html/signup.php
+++ html/signup.php
@@ -55,13 +55,14 @@ if($name == ''){
 //echo '올바른 이메일 주소가 맞습니다.';
 
 //이제부터 넣기 시작
-$sql = "INSERT INTO user VALUES(1, '$id','$password','$email')";
+$userNum = "SELECT COUNT(user) FROM easyhomecafeDB" + 1;
+$sql = "INSERT INTO user VALUES('$userNum', '$id','$password','$email')";
 
 if($mysqli_connect->query($sql) === true){
     //echo '회원가입 성공';
     echo "<script>location.href='login.php'</script>";
 }else{
-    echo mysqli_error($mysqli_connect);
-    echo "실패";
+    //echo mysqli_error($mysqli_connect);
+    //echo "실패";
 }
 ?>
