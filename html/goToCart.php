579a952d118ad70a33010cefacf24d14d0a9544e
GuGaYoung <rkdudda12@naver.com>
2020-06-30 15:59:43 +0900

    title이 EasyHomeCafe-로 시작할 수 있도록 수정

--- html/goToCart.php
+++ html/goToCart.php
@@ -6,7 +6,7 @@ $mysqli_connect = connect_Mysqli();
 <html>
 <head>
     <meta charset="utf-8" />
-    <title>쇼핑몰 장바구니</title>
+    <title>EasyHomeCafe-장바구니</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="goToCart.css" />
 </head>
--- html/login.php
+++ html/login.php
@@ -7,7 +7,7 @@ include "headerNav.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EasyHomeCafe 로그인</title>
+    <title>EasyHomeCafe-로그인</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,600&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/264954fef8.js" crossorigin="anonymous"></script>
