5a0dcd06394a7f0d8c47036a12248e1a32639c86
GuGaYoung <rkdudda12@naver.com>
2020-07-01 11:06:43 +0900

    상품결제 기능 추가

--- html/goToCart.php
+++ html/goToCart.php
@@ -7,6 +7,8 @@ $mysqli_connect = connect_Mysqli();
 <head>
     <meta charset="utf-8" />
     <title>EasyHomeCafe-장바구니</title>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js" ></script>
+    <script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.1.5.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="goToCart.css" />
 </head>
@@ -20,6 +22,7 @@ $mysqli_connect = connect_Mysqli();
             <th>개수</th>
             <th>상품금액</th>
             <th>등록일</th>
+            <th></th>
         </tr>
         <?php
         session_start();
@@ -39,6 +42,7 @@ $mysqli_connect = connect_Mysqli();
                 <td><?php echo $row['quantity'];?>개</td>
                 <td><?php echo $row['price']*$row['quantity'];?>원</td>
                 <td><?php echo $row['date']; ?></td>
+                <td><button class="btn btn-secondary" onClick="location.href='orderedProductDelete.php?idx=<?php echo $row['idx'];?>'">삭제</button></td>
             </tr>
             </tbody>
             <?php
@@ -49,10 +53,11 @@ $mysqli_connect = connect_Mysqli();
         echo "<div class=\"arrangement\">";
         echo "<span class=\"finalQuantity\" >총개수 : $finalQuantity 개</span>";
         echo "<span class=\"finalPrice\" >총가격 : $finalPrice 원</span>";
-        echo "<button class=\"paymentBtn btn btn-secondary\">결제하기</button>";
+        echo "<button class=\"paymentBtn btn btn-secondary\" onClick=\"paymentBtn_click($finalPrice);\" >결제하기</button>";
         echo "</div>";
         ?>
+
 </div>
-<footer></footer>
+<script src="payment.js"></script>
 </body>
 </html>
