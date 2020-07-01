c1c1b0b1ad76d9fc5ad625a3d019a1a76bc7fd73
GuGaYoung <rkdudda12@naver.com>
2020-07-01 17:43:59 +0900

    아이디 찾기 레이아웃 제작

--- html/login.php
+++ html/login.php
@@ -9,8 +9,6 @@ include "headerNav.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EasyHomeCafe-로그인</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,600&display=swap" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/264954fef8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="login.css">
     <script defer src="login.js"></script>
 </head>
@@ -36,7 +34,7 @@ include "headerNav.php";
                 <span class="keepLogin">로그인 상태 유지</span>
             </div>
             <div class="mx-auto form-group">
-                <a href="/member/find/loginId" class="linkFind">아이디</a>
+                <a href="findId.php" class="linkFind">아이디</a>
                 <a href="/member/find/password" class="linkFind">비밀번호 찾기</a>
                 <a href="signup.html" class="signUp">회원가입</a>
             </div>
