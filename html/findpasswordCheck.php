f31589c3270ebae9e2a07829a698c714d0db4506
GuGaYoung <rkdudda12@naver.com>
2020-07-01 22:17:17 +0900

    비밀번호 찾기 구현

---
+++ html/findpasswordCheck.php
@@ -0,0 +1,45 @@
+<?php
+    include "mysqlConnect.php";
+    $mysqli_connect = connect_Mysqli();
+
+    $id = $_POST['inputId'];
+    $name = $_POST['inputName'];
+    $email = $_POST['inputEmail'];
+
+    $query = "select * from user where id='$id' AND name='$name' AND email='$email'";
+    $result = mysqli_query($mysqli_connect, $query);
+    $row = mysqli_fetch_array($result);
+    $password = $row['password'];
+
+    if($row == null){
+        echo '<script type="text/javascript">alert("해당하는 회원정보를 찾을 수 없습니다.");</script>';
+    }else{
+
+    $password = passwordGenerater();
+    $encrypted_passwd = password_hash($password, PASSWORD_DEFAULT);
+    echo "<script>alert('새 비밀번호는 {$password}입니다.');</script>";
+
+    $sql = "UPDATE user SET password='$encrypted_passwd' WHERE id='$id' AND name='$name' AND email='$email'";
+
+    if($mysqli_connect->query($sql) === true){
+        echo "<script>location.href='login.php';</script>";
+    }else{
+        //echo mysqli_error($mysqli_connect);
+        //echo "실패";
+    }
+}
+
+function passwordGenerater() {
+    $length = 10;
+    $characters = "0123456789";
+    $characters .= "abcdefghijklmnopqrstuvwxyz";
+    $characters .= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $string_generated = "";
+    $nmr_loops = $length;
+    while ($nmr_loops--) {
+        $string_generated .= $characters[mt_rand(0, strlen($characters))];
+    }
+    return $string_generated;
+}
+
+?>
