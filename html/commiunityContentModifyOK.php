64ccff75d76a7b782628c934e6c07e98f8658867
GuGaYoung <rkdudda12@naver.com>
2020-07-02 09:53:32 +0900

    commiunityContentModifyOK.php 코드 정리

--- html/commiunityContentModifyOK.php
+++ html/commiunityContentModifyOK.php
@@ -2,16 +2,23 @@
 include "headerNav.php";
 $mysqli_connect = connect_Mysqli();
 
+//사용자가 작성한 게시물 정보를 가져온다.
 $title = $_POST["title"];
 $content = $_POST["content"];
 $contentsNum = $_GET['idx'];
 
+//이미지를 넣었다면
 if(basename($_FILES["image"]["name"]) != null){
+
+    //이미지의 경로를 정하고 이미지 이름을 수정
+    //이미지의 이름을 곂치지 않게 하기 위해 원래이미지의 이름 + 날짜,시분초로 수정
     $target_dir = "image/";
     $target_file = $target_dir.date("Y.m.d.H:i:s").basename($_FILES["image"]["name"]);
+    //업로드가 제대로 됬다면 1 아니라면 0
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
+    //이미지가 아닌 다른 파일을 넣었을때의 처리
     if(isset($_POST["submit"])) {
         $check = getimagesize($_FILES["image"]["tmp_name"]);
         if($check !== false) {
@@ -23,24 +30,26 @@ if(basename($_FILES["image"]["name"]) != null){
         }
     }
 
-    // Check file size
+    //이미지 크기에 따른 처리
     if ($_FILES["image"]["size"] > 500000) {
         echo '<script type="text/javascript">alert("파일의 크기가 너무 큽니다.");</script>';
         $uploadOk = 0;
     }
 
-    // Allow certain file formats
+    //이미지 타입에 따른 처리
     if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
         && $imageFileType != "gif" ) {
         echo '<script type="text/javascript">alert("JPG, JPEG, PNG & GIF 이미지만 등록할 수 있습니다.");</script>';
         $uploadOk = 0;
     }
 
-    // Check if $uploadOk is set to 0 by an error
+    // 이미지 업로드 조건에 충족했는지에 따른 처리
     if ($uploadOk == 0) {
+        //충족하지 못했다면 다시 설정할 수 있도록 전 페이지로 이동
         echo '<script type="text/javascript">alert("파일이 업로드가 되지 않았습니다.");</script>';
         echo "<script>history.back();</script>";
-        // if everything is ok, try to upload file
+
+        //충족했다면 이미지와 게시물 정보들을 데이터베이스 안에 넣는다.
     } else {
         if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
 
@@ -57,9 +66,9 @@ if(basename($_FILES["image"]["name"]) != null){
             echo '<script type="text/javascript">alert("파일이 업로드 되는 중에 에러가 발생했습니다.");</script>';
         }
     }
-
+    //이미지를 넣지 않고 게시물을 작성했다면
 }else{
-
+    //게시물 정보들을 데이터베이스 안에 넣는다.
     $sql = "UPDATE commiunity SET title='$title', content ='$content' WHERE idx = '$contentsNum'";
 
     if($mysqli_connect->query($sql) === true){
