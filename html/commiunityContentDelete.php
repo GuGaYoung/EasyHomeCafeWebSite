804ac920c02ffd373365c50e1b16b3cfce87cacb
GuGaYoung <rkdudda12@naver.com>
2020-07-02 09:52:58 +0900

    commiunityContentDelete.php 코드 정리

--- html/commiunityContentDelete.php
+++ html/commiunityContentDelete.php
@@ -1,9 +1,9 @@
 <?php
 include "headerNav.php";
+
+//인덱스에 해당하는 게시물을 찾아 삭제한다.
 $mysqli_connect = connect_Mysqli();
-
 $contentsNum = $_GET['idx'];
-
 $sql = "DELETE FROM commiunity where idx='$contentsNum'";
 
 if($mysqli_connect->query($sql) === true){
