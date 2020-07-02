0c4c14d8bf0ab2ec3511db231e5a74e202679d60
GuGaYoung <rkdudda12@naver.com>
2020-07-02 10:07:01 +0900

    findIdCheck.php 코드 정리

--- html/findIdCheck.php
+++ html/findIdCheck.php
@@ -5,15 +5,16 @@ $mysqli_connect = connect_Mysqli();
 $name = $_POST['inputName'];
 $email = $_POST['inputEmail'];
 
+//작성한 이메일과 이름에 해당하는 아이디를 찾는다.
 $query = "select * from user where name='$name' AND email='$email'";
 $result = mysqli_query($mysqli_connect, $query);
 $row = mysqli_fetch_array($result);
 $id = $row['id'];
 
 if($row == null){
-    echo '<script type="text/javascript">alert("해당하는 아이디를 찾을 수 없습니다.");</script>';
+    echo '<script type="text/javascript">alert("해당하는 회원정보를 찾을 수 없습니다.");</script>';
 }else{
-    echo "<script>alert('아이디 : {$id}');</script>";
+    echo "<script>alert('아이디는 {$id}입니다.');</script>";
     echo "<script>location.href='login.php';</script>";
 }
 ?>
