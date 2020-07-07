d983d187cdf8aa6244aa457038b6d5d26a89a611
GuGaYoung <rkdudda12@naver.com>
2020-07-07 12:16:39 +0900

    replyDeleteOK.php 코드 정리

--- html/replyDeleteOK.php
+++ html/replyDeleteOK.php
@@ -4,11 +4,11 @@ $mysqli_connect = connect_Mysqli();
 
 $replyNum = $_GET['idx'];
 
+//선택한 idx의 댓글을 삭제한다.
 $sql = "DELETE FROM commiunity_reply where idx='$replyNum'";
 
 if($mysqli_connect->query($sql) === true){
     echo "<script>history.back();</script>";
-    //header("location: commiunity.php");
 }else{
     //echo mysqli_error($mysqli_connect);
 }
