eab393920df3df45846eb78fc2cf5cb5ec03686d
GuGaYoung <rkdudda12@naver.com>
2020-07-07 12:16:17 +0900

    popup.js 코드 정리

--- html/popup.js
+++ html/popup.js
@@ -3,10 +3,11 @@ function setCookie(name, value, expiredays){
     todayDate.setDate (todayDate.getDate() + expiredays);
     document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toGMTString() + ";";
 }
-
+//팝업창에서 1일동안 이 창을 보지 않음 버튼을 누르면
+//쿠키에 하루동안 팝업창을 띄우지 않게하기 위해 쿠키에 값을 저장해놓음
 function closePop(){
     if (document.frm.pop.checked){
-        setCookie("popname", "done", 1);
+        setCookie("isPopUpDeleted", "done", 1);
     }
     self.close();
 }
